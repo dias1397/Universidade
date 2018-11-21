172b7fafc2035e299f56cbb426e1ad8d447e5258
dias1397 <joao_dias1397@hotmail.com>
2018-11-21 17:00:10 +0000

    Update ast.h

--- Mestrado/TAC/trabalho1/ast.h
+++ Mestrado/TAC/trabalho1/ast.h
@@ -11,7 +11,9 @@ typedef struct variable_declaration *variable_declaration;
 typedef struct function_declaration *function_declaration;
 
 typedef struct identifier *identifier;
+
 typedef struct expression *expression;
+typedef struct expressions *expressions;
 
 typedef struct formal_arg * formal_arg;
 typedef struct formal_args *formal_args;
@@ -21,6 +23,13 @@ typedef struct body *body;
 typedef struct kind *kind;
 typedef struct type *type;
 
+typedef struct statement *statement;
+typedef struct statements *statements;
+typedef struct if_statement *if_statement;
+typedef struct call_statement *call_statement;
+
+typedef struct fun_call *fun_call;
+typedef struct atomic_expression *atomic_expression;
 
 enum kind_ {VAR_, LOCAL_, ARG_};
 enum type_ {INT_, REAL_, BOOL_};
@@ -131,3 +140,76 @@ struct body
 	statement arg1;
 	expression arg2;
 };
+
+struct kind{
+};
+
+struct type{
+};
+
+struct statement{
+	enum{assign_, call_, print_, if_, while_, stmts_} kind;
+	
+	union{
+		identifier arg0;
+		expression arg1;
+	}assign_;
+	union{
+		call_statement arg0;
+	}call_;
+	union{
+		expression arg0;
+	}print_;
+	union{
+		if_statement arg0;
+	}if_;
+	union{
+		expression arg0;
+		statement arg1;
+	}while_;
+	union{
+		statements arg0;
+	}stmts_;
+};
+
+struct statements{
+	statement arg0;
+	statements arg1;
+};
+
+struct call_statement{
+	union{
+		char *arg0;
+	}u1;
+	union{
+		identifier arg0;
+	}u2;
+	
+	expressions arg1;
+};
+
+struct if_statement{
+	expression arg0;
+	statement arg1;
+	statement arg2;
+};
+
+struct expressions{
+	expression arg0;
+	expressions arg1;
+};
+
+struct atomic_expression{
+	identifier arg0;
+	fun_call arg1;
+	int arg2;
+};
+
+struct fun_call{
+	char *arg0;
+	expressions arg1;
+};
+
+
+
+
